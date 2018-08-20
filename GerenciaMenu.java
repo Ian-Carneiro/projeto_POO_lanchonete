212410fee060632bcf6accbebe3a5d0972565d3d
Leanderson-Coelho <34748960+Leanderson-Coelho@users.noreply.github.com>
2018-08-20 16:56:22 -0300

    Melhorando os comentários e renomeando Classe

---
+++ GerenciaMenu.java
@@ -0,0 +1,55 @@
+package com.lanchonete.model;
+
+import java.util.ArrayList;
+
+public class GerenciaMenu {
+	private ArrayList<Produto> produtos;
+	
+	public GerenciaMenu() {
+		produtos = new ArrayList<>();
+	}
+	
+	//buscar o indice do produto na lista pelo seu codigo
+	public int buscarProduto(int codigo) {
+		for(int i = 0; i<produtos.size();i++) {
+			if(produtos.get(i).getCodigo() == codigo) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	//buscar e retornar o produto
+	public Produto buscarProduto(Produto produto) {
+		for(Produto p: produtos) {
+			if(p.equals(produto))
+				return p;
+		}
+		return null;
+	}
+	
+	public boolean adicionarProduto(Produto produto) {
+		if(buscarProduto(produto.getCodigo()) >=0 ) {
+			return false;//Produto com codigo ja cadastrado
+		}
+		return produtos.add(produto);
+	}
+	
+	public boolean excluirProduto(int codigo) {
+		if(buscarProduto(codigo)<0)//verifica se o produto existe
+			return false;
+		return produtos.remove(produtos.get(buscarProduto(codigo)));
+	}
+	
+	//recebe o codigo do produto que sera editado e um novo produto com informações novas do produto
+	public boolean editarProduto(int codigo, Produto produto) {
+		if(buscarProduto(codigo)<0)//verifica se o produto existe para ser editado
+			return false;
+		produto.setCodigo(codigo);//garante que o produto editado(novo produto) tenha o mesmo codigo
+		if(excluirProduto(codigo)) {//se excluit o produto antigo sera adicionado o novo(editado)
+			adicionarProduto(produto);//adicionando o produto editado
+			return true;
+		}
+		return false;
+	}
+}
